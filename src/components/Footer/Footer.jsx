097bda5ecf87f0bd694445f4f596e01be3c2ab91
amalfrost <amalrajks6@gmail.com>
2023-09-17 00:37:43 +0530

    added footer-icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './Footer.scss'
+import { Link } from 'react-router-dom'
+import{FaGithub, FaLinkedin} from 'react-icons/fa'
 
 function Footer() {
   return (
     <div className='footer'>
-      <div>Movie App</div>
-      <div>AmalRaj &co</div>
+    
+      <Link style={{ color: 'inherit', textDecoration: 'inherit'}}><div>AmalRaj </div> </Link>
+
+     <div className='footer-icons'>
+     <Link><FaGithub className='icon'/></Link>
+      <Link><FaLinkedin className='icon'/></Link>
+     </div>
     </div>
   )
 }
